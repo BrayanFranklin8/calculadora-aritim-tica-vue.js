0bf132254a94640667e1e5fb814d5ed544ce537f
BRAYAN <brayanfranklin88@gmail.com>
2025-07-16 06:21:13 -0300

    finalizando estilização

--- src/App.vue
+++ src/App.vue
@@ -50,13 +50,16 @@ import { reactive } from 'vue';
     <form>
       <input @keyup="evento => {numero.numero1 = evento.target.value; calcula()}"  class="m-1"  type="number" placeholder="insira o numero">
       <select @change="evento => {numero.operador = evento.target.value;calcula();}" class="m-1 p-1" >
-          <option value="+">+</option>
+          <option value="+"><i class="bi bi-plus-circle">+</i></option>
           <option value="-">-</option>
           <option value="*">*</option>
           <option value="/">/</option>
       </select>
       <input @keyup="evento => {numero.numero2 = evento.target.value;calcula()}" class="m-1" type="number" placeholder="insira o numero">
-      = {{ numero.resultado }}
+      <img src="./images/equal1.png" alt="simbolo do igual">
+      <div class="resultado">
+        {{ numero.resultado }}
+      </div>
     </form>
   </div>
 </div>
@@ -72,12 +75,22 @@ import { reactive } from 'vue';
   border-radius: 30px;
   background-color: #bc92d5;
   border: 1px solid black;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
 }
 
 input{
   height: 34px;
-
+  background-color: #f5d8ff;
+  border: 2px solid #a37ac0;
 }
+
+select{
+  font-size: 20px;
+  height: 34px;
+  background-color: #f5d8ff;
+  border: 2px solid #a37ac0;
+}
+
 
 header{
   display: flex;
@@ -90,11 +103,28 @@ form{
   align-items: center;
   justify-content: center;
   padding: 50px;
-  background-color: rgb(174, 228, 223);
+  background-color: #d5a9ea;
+  border-bottom-right-radius:30px ;
+  border-bottom-left-radius:30px ;
+}
 
+img{
+  width: 30px;
+  height: 30px;
+  margin: 0 12px;
 }
 
 h1{
   margin: 0 auto;
+}
+
+.resultado{
+  width: 32px;
+  height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #a37ac0;
+  background-color:#f5d8ff;
 }
 </style>
